fbd4c3141b1e0fcd9abfb860ea1a5dd24892eca4
Kapifun <kirill.doronin.07@mail.ru>
2024-11-21 12:14:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-var_int = 10
-var_float = 8.4
-var_str = "No"
-big_int35 = var_int * 3.5
-var_float -= 1
-print(var_int / var_float)
-print(big_int35 / var_float)
-var_str = var_str * 2 + "Yes" * 3
-print(var_int, var_float, big_int35, var_str)
+var_int = 10 #вводим переменную и присваиваем ей число
+var_float = 8.4 #вводим переменную и присваиваем ей число
+var_str = "No" #вводим переменную и присваиваем ей слово
+big_int35 = var_int * 3.5 #присваиваем переменной значение var_int умноженное на 3,5
+var_float -= 1 #отнимает от переменной var_float 1
+print(var_int / var_float) #выводим var_int делённое на var_float
+print(big_int35 / var_float) #выводим big_int35 делённое на var_float
+var_str = var_str * 2 + "Yes" * 3 #присваиваиваем переменной новое значение
+print(var_int, var_float, big_int35, var_str) #выводим остальные переменные
